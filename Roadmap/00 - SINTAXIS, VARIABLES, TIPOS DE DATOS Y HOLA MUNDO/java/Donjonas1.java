6ce7fd950f4ff0f62c13ccefef2f518f099b96cd
DonJonas1 <jromerol769@gmail.com>
2024-06-23 16:15:51 -0600

    Create Donjonas1.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Donjonas1.java
@@ -0,0 +1,15 @@
+  // Buenas, aquí la url: https://www.java.com/es/ comentario 1 linea//
+        /*Comentario 
+             varias
+             lineas*/
+        
+    
+      String variable1="";
+     final double PI =3.141592564;   // Constante//
+      String cadena="java";
+       boolean bol=false;
+       int entero= 1;
+       double real= 3455.34;
+       long lg= 2343385;
+         System.out.println("¡Hola!,"+cadena);
+        
